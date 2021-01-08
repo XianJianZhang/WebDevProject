59f4bd2170abf37c73142886e4e02b0e7c33d8f3
Andy <xianjianzhang.yahoo@gmail.com>
2021-01-08 09:16:44 -0600

    Created home and about page with templates, also a design page for front development

---
+++ webdev/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.homepage, name='blog-home'),
+  path('about/', views.about, name='blog-about'),
+  path('design/', views.design, name='blog-design'),
+]
--- webdev/blog/views.py
+++ webdev/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'blog/home.html', context={'title':'Homepage'})
+
+def about(request):
+  return render(request, 'blog/about.html', context={'title':'About page'})
+
+def design(request):
+  return(render(request, 'blog/design.html', context={'title':'Design'}))
